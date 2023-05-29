07ce92c1545366aed4b03f23138811b2d53e71e4
pmosk <pm@ggroupp.ru>
2023-05-29 22:27:51 +0400

    Create first version

---
+++ src/AzureFunc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace GarageGroup.Platform.Swagger.Hub;
+
+static class Program
+{
+    static Task Main()
+        =>
+        Host.CreateDefaultBuilder()
+        .ConfigureFunctionsWorkerStandard(
+            useHostConfiguration: true)
+        .Build()
+        .RunAsync();
+}
